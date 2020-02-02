1140909a2c2724755142d9b5c403d51655162c4d
Dan Park <dpark.sports@gmail.com>
2020-02-01 18:22:37 -0800

    released v3.5

---
+++ CamController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  SecurityCam
+//
+//  Created by Dan Park on 11/11/14.
+//  Copyright (c) 2014 MagicPoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <QuartzCore/QuartzCore.h>
+#import <CoreText/CoreText.h>
+#import <Photos/Photos.h>
+#import "sys/stat.h"
+
+@interface CamController : UIViewController
+
+
+@end
+
