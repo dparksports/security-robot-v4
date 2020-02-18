fc5cd783ed008408ed0739a420ac6d596a415e62
Dan Park <dpark.sports@gmail.com>
2020-02-17 23:28:08 -0800

    changed dim screen at 30%

--- AppDelegate.m
+++ AppDelegate.m
@@ -35,7 +35,7 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     UIScreen *screen = [UIScreen mainScreen];
-    screen.brightness = 0.5;
+    screen.brightness = 1/3.0;
     
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -55,6 +55,8 @@
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
+    UIScreen *screen = [UIScreen mainScreen];
+    screen.brightness = 1/3.0;
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
